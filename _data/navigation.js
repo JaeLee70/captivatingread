26c98a69d4cfc3d0bc88151819ff4ec1573d2240
JaeLee70 <junroolee@gmail.com>
2025-04-27 00:51:00 +0200

    Create navigation.js

---
+++ _data/navigation.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    title: "Accueil",
+    url: "/"
+  },
+  {
+    title: "À propos",
+    url: "/a-propos/"
+  },
+  {
+    title: "Blog",
+    url: "/blog/"
+  },
+  {
+    title: "Contact",
+    url: "/contact/"
+  }
+];
